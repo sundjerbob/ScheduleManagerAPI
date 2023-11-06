e11ff7eaa2165e0294e39598dd2695f59f9415ca
Tadija <tadijasimic999@gmail.com>
2023-11-06 02:15:59 +0100

    persistence unit for file writing and creating new files

---
+++ src/main/java/raf/sk_schedule/util/persistence/ScheduleFileWriter.java
@@ -0,0 +1,47 @@
+package raf.sk_schedule.util.persistence;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ScheduleFileWriter {
+
+
+    public static File createFileIfNotExists(String filePath) {
+        File file = new File(filePath);
+
+        if (!file.exists()) {
+            // Get the file name from the path
+            String fileName = file.getName();
+
+            try {
+                // Create a new file in the same directory with the extracted name
+                File newFile = new File(file.getParent(), fileName);
+
+                if (newFile.createNewFile()) {
+                    System.out.println("File created: " + newFile.getAbsolutePath());
+                    return newFile; // Return the newly created File object
+                } else {
+                    System.err.println("Failed to create the file.");
+                }
+            } catch (IOException e) {
+                System.err.println("An error occurred: " + e.getMessage());
+            }
+        }
+
+        return file; // If the file already exists, return the existing File object
+    }
+
+    public static void writeStringToFile(File file, String content) {
+        try {
+            FileWriter fileWriter = new FileWriter(file, true); // 'true' enables append mode
+            BufferedWriter writer = new BufferedWriter(fileWriter);
+            writer.write(content);
+            writer.newLine(); // Add a newline character after the content
+            writer.close();
+        } catch (IOException e) {
+            System.err.println("An error occurred while writing to the file: " + e.getMessage());
+        }
+    }
+}
