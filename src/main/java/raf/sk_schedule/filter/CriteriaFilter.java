8045072293e0fe465233c0a0c0cecbabe8dc42f6
Tadija <tadijasimic999@gmail.com>
2023-11-19 06:59:00 +0100

    clean the garbage imports

--- src/main/java/raf/sk_schedule/filter/CriteriaFilter.java
+++ src/main/java/raf/sk_schedule/filter/CriteriaFilter.java
@@ -1,35 +1,32 @@
 package raf.sk_schedule.filter;
 
-import raf.sk_schedule.api.Constants;
-import raf.sk_schedule.exception.ScheduleException;
-import raf.sk_schedule.model.location.RoomProperties;
 import raf.sk_schedule.model.schedule.ScheduleSlot;
 
-import java.util.Date;
-import java.util.Map;
-
-import static raf.sk_schedule.util.format.DateTimeFormatter.parseDate;
-import static raf.sk_schedule.util.format.DateTimeFormatter.parseTime;
 
 /**
  * Functional interface for implementing custom filters.
  */
 public interface CriteriaFilter {
 
-    // Constants for criteria keys
-    public static final int DATE_KEY = 0;
-    public static final int WEEK_DAY_KEY = 1;
-    public static final int START_TIME_KEY = 3;
-    public static final int END_TIME_KEY = 4;
-    public static final int DURATION_KEY = 2;
-    public static final int LOCATION_KEY = 5;
-    public static final int DYNAMIC_ATTRIBUTES_KEY = 6;
-    public static final int UPPER_BOUND_DATE_KEY = 7;
-    public static final int LOWER_BOUND_DATE_KEY = 8;
+    /**
+     * Constants representing keys for various criteria.
+     */
+    int DATE_KEY = 0;
+    int WEEK_DAY_KEY = 1;
+    int START_TIME_KEY = 3;
+    int END_TIME_KEY = 4;
+    int DURATION_KEY = 2;
+    int LOCATION_KEY = 5;
+    int DYNAMIC_ATTRIBUTES_KEY = 6;
+    int UPPER_BOUND_DATE_KEY = 7;
+    int LOWER_BOUND_DATE_KEY = 8;
 
+    /**
+     * Filters a schedule slot based on the specified criteria.
+     *
+     * @param slot           The schedule slot to be filtered.
+     * @param searchCriteria The criteria used for filtering.
+     * @return True if the slot satisfies the criteria, false otherwise.
+     */
     boolean filter(ScheduleSlot slot, SearchCriteria searchCriteria);
-
-
-
-
 }
